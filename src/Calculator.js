a779903fb0ad10a168ce335fb88e743ebd06fd54
tangweikun <819573105@qq.com>
2019-11-22 13:22:49 +0800

    :lipstick: update ui

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
 import { CalculatorDisplay } from "./CalculatorDisplay";
 import { CalculatorKey } from "./CalculatorKey";
@@ -18,30 +18,12 @@ export function Calculator() {
   const [displayValue, setDisplayValue] = useState("0");
   const [operator, setOperator] = useState(null);
   const [waitingForOperand, setWaitingForOperand] = useState(false);
-  const clearDisplayFoo = displayValue !== "0";
-  const clearText = clearDisplayFoo ? "C" : "AC";
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", this.handleKeyDown);
-    };
-  }, []);
 
   function clearAll() {
     setValue(null);
     setDisplayValue("0");
     setOperator(null);
     setWaitingForOperand(false);
-  }
-
-  function clearDisplay() {
-    setDisplayValue("0");
-  }
-
-  function clearLastChar() {
-    setDisplayValue(displayValue.substring(0, displayValue.length - 1) || "0");
   }
 
   function toggleSign() {
@@ -93,47 +75,13 @@ export function Calculator() {
     setOperator(nextOperator);
   }
 
-  function handleKeyDown(event) {
-    let { key } = event;
-
-    if (key === "Enter") key = "=";
-
-    if (/\d/.test(key)) {
-      event.preventDefault();
-      inputDigit(parseInt(key, 10));
-    } else if (key in CalculatorOperations) {
-      event.preventDefault();
-      performOperation(key);
-    } else if (key === ".") {
-      event.preventDefault();
-      inputDot();
-    } else if (key === "%") {
-      event.preventDefault();
-      inputPercent();
-    } else if (key === "Backspace") {
-      event.preventDefault();
-      clearLastChar();
-    } else if (key === "Clear") {
-      event.preventDefault();
-
-      if (displayValue !== "0") {
-        clearDisplay();
-      } else {
-        clearAll();
-      }
-    }
-  }
-
   return (
     <div className="calculator">
       <CalculatorDisplay value={displayValue} />
       <div className="calculator-keypad">
         <div className="input-keys">
           <div className="function-keys">
-            <CalculatorKey
-              onPress={() => (clearDisplayFoo ? clearDisplay() : clearAll())}
-              text={clearText}
-            />
+            <CalculatorKey onPress={() => clearAll()} text="AC" />
             <CalculatorKey text="±" onPress={() => toggleSign()} />
             <CalculatorKey text="%" onPress={() => inputPercent()} />
           </div>
@@ -156,13 +104,9 @@ export function Calculator() {
         </div>
         <div className="operator-keys">
           <CalculatorKey text="÷" onPress={() => performOperation("/")} />
-
           <CalculatorKey text="×" onPress={() => performOperation("*")} />
-
           <CalculatorKey text="-" onPress={() => performOperation("-")} />
-
           <CalculatorKey text="+" onPress={() => performOperation("+")} />
-
           <CalculatorKey text="=" onPress={() => performOperation("=")} />
         </div>
       </div>
